cc1ccfda63944c64fcea3ca128f32cab5e9f4acc
MSPFE2019 <57810061+MSPFE2019@users.noreply.github.com>
2019-12-09 15:37:38 -0600

    Update Dismount_DB.ps1

--- Dismount_DB.ps1
+++ Dismount_DB.ps1
@@ -9,41 +9,20 @@
 # creation, production, or delivery of the scripts be liable for any damages whatsoever (including, without
 # limitation, damages for loss of business profits, business interruption, loss of business information,
 # or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, 
-# even if Microsoft has been advised of the possibility of such damages.
+# even if Microsoft has been advised of the possibility of such damages.
 ##########################################################################################################
 
 #Add SharePoint PowerShell Snap-In 
-if ((Get-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue)-eq $null) {Add-PSSnapin "Microsoft.SharePoint.PowerShell"} 
+			if ((Get-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue) -eq $null) { 
+			Add-PSSnapin "Microsoft.SharePoint.PowerShell"} 
 
-####Import CSV file from current directory 
-  Import-Csv "contentdbs.csv"| % {
-   $MaxThreads = 4 ###Number of Upgrades that will be ran at a time - max 10
-   while (@(Get-Job | Where { $_.State -eq "Running" }).Count -ge $MaxThreads)
-   {  Write-Host "Waiting for open thread...($MaxThreads Maximum)"
-      Start-Sleep -Seconds 3
-   }
-   ### Mount of the database - trigger database upgrade
-  $ScriptBlock = {
-    param($Name,$webapp,$NormalizedDataSource) 
-    
-    Add-PSSnapin "Microsoft.SharePoint.PowerShell"
-    Mount-SPContentDatabase -WebApplication $name.webapp -Name $Name.name -DatabaseServer $name.NormalizedDataSource
-    Start-Sleep 50
-  }
+$dbs = Get-SPContentDatabase
+        if ($dbs) {
+            $dbs | % {$wa = $_.WebApplication.Url; $_ | select Name, NormalizedDataSource, @{n = "WebApp"; e = {$wa}}} | Export-Csv "contentdbs.csv" -NoTypeInformation -force
+            $dbs | % {
+                "$($_.Name),$($_.NormalizedDataSource)"
+                Dismount-SPContentDatabase $_ -Confirm:$false
+            }
+        }
 
-  # Execute the jobs in parallel
-  Start-Job -Name $_.name $ScriptBlock -ArgumentList $_
-}
-while (@(Get-Job | Where { $_.State -eq "Running" }).Count -ne 0) ##Check for running jobs
-{  Write-Host "Waiting for background jobs..."
-   Get-Job    #Just showing all the jobs
-   Start-Sleep -Seconds 3
-}
- 
-Get-Job       #Just showing all the jobs
-$Data = foreach ($Job in (Get-Job)) {
-   Receive-Job $Job
-   Remove-Job $Job
-}
- 
-$Data | Select Name, PSBeginTime, PSEndTime
+
