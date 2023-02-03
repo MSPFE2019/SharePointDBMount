96c4e2bb698fdfdd2a2debff07540dc20e195cea
Mose <57810061+MSPFE2019@users.noreply.github.com>
2023-02-02 22:51:27 -0600

    Update mount_db.ps1

--- mount_db.ps1
+++ mount_db.ps1
@@ -1,50 +1,44 @@
-##########################################################################################################
-# Disclaimer
-# The sample scripts are not supported under any Microsoft standard support program or service.
-#
-# The sample scripts are provided AS IS without warranty of any kind. Microsoft further disclaims all
-# implied warranties including, without limitation, any implied warranties of merchantability or of fitness
-# for a particular purpose. The entire risk arising out of the use or performance of the sample scripts and
-# documentation remains with you. In no event shall Microsoft, its authors, or anyone else involved in the
-# creation, production, or delivery of the scripts be liable for any damages whatsoever (including, without
-# limitation, damages for loss of business profits, business interruption, loss of business information,
-# or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, 
-# even if Microsoft has been advised of the possibility of such damages.
-##########################################################################################################
-
-#Add SharePoint PowerShell Snap-In 
-if ((Get-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue)
--eq $null) {Add-PSSnapin "Microsoft.SharePoint.PowerShell"} 
-
-####Import CSV file from current directory 
-  Import-Csv "contentdbs.csv"| % {
-   $MaxThreads = 4 ###Number of Upgrades that will be ran at a time - max 10
-   while (@(Get-Job | Where { $_.State -eq "Running" }).Count -ge $MaxThreads)
-   {  Write-Host "Waiting for open thread...($MaxThreads Maximum)"
-      Start-Sleep -Seconds 3
-   }
-   ### Mount of the database - trigger database upgrade
-  $ScriptBlock = {
-    param($Name,$webapp,$NormalizedDataSource) 
-    
+# Add SharePoint PowerShell Snap-In 
+if (!(Get-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue)) {
     Add-PSSnapin "Microsoft.SharePoint.PowerShell"
-    Mount-SPContentDatabase -WebApplication $name.webapp -Name $Name.name -DatabaseServer $name.NormalizedDataSource
-    Start-Sleep 50
-  }
+}
 
-  # Execute the jobs in parallel
-  Start-Job -Name $_.name $ScriptBlock -ArgumentList $_
+# Import CSV file from current directory
+$contentDBs = Import-Csv "contentdbs.csv"
+
+# Maximum number of upgrades that will be run at a time
+$MaxThreads = 4
+
+# Define the script block for the upgrade process
+$ScriptBlock = {
+    param ($Name, $WebApp, $NormalizedDataSource)
+
+    Add-PSSnapin "Microsoft.SharePoint.PowerShell"
+    Mount-SPContentDatabase -WebApplication $WebApp -Name $Name -DatabaseServer $NormalizedDataSource
+    Start-Sleep 50
 }
-while (@(Get-Job | Where { $_.State -eq "Running" }).Count -ne 0)
-{  Write-Host "Waiting for background jobs..."
-   Get-Job    #Just showing all the jobs
-   Start-Sleep -Seconds 3
+
+# Execute the upgrades in parallel
+foreach ($contentDB in $contentDBs) {
+    while (@(Get-Job | Where { $_.State -eq "Running" }).Count -ge $MaxThreads) {
+        Write-Host "Waiting for open thread...($MaxThreads Maximum)"
+        Start-Sleep -Seconds 3
+    }
+    Start-Job -Name $contentDB.Name -ScriptBlock $ScriptBlock -ArgumentList $contentDB.Name, $contentDB.webapp, $contentDB.NormalizedDataSource
 }
- 
-Get-Job       #Just showing all the jobs
+
+# Wait for all jobs to complete
+while (@(Get-Job | Where { $_.State -eq "Running" }).Count -ne 0) {
+    Write-Host "Waiting for background jobs..."
+    Get-Job
+    Start-Sleep -Seconds 3
+}
+
+# Get results and remove the jobs
 $Data = foreach ($Job in (Get-Job)) {
-   Receive-Job $Job
-   Remove-Job $Job
+    Receive-Job $Job
+    Remove-Job $Job
 }
- 
+
+# Display the results
 $Data | Format-Table -AutoSize
